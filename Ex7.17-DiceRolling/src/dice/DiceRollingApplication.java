95730294dc00874ab1756a58c64fcac66a537857
fsdam <fsdam@DESKTOP-58ASB03.lan>
2022-08-26 17:01:29 +0100

    Added Print statement

--- Ex7.17-DiceRolling/src/dice/DiceRollingApplication.java
+++ Ex7.17-DiceRolling/src/dice/DiceRollingApplication.java
@@ -5,6 +5,7 @@ public class DiceRollingApplication {
 	public static void main(String[] args) {
 		DiceRoller roll = new DiceRoller();
 		roll.displayResults();
+		System.out.println("Dice");
 	}
 
 }
